0af0f4c1eadbc4c1ea928e4395e3fcaac67e8e28
xms <544437819@qq.com>
2021-02-23 13:21:33 +0800

    youhua

--- constant.js
+++ constant.js
@@ -1,2 +1,4 @@
+const { dependencies } = require('./package.json');
+
 exports.pluginName = 'i18n-transform';
-exports.webpackPluginName = 'webpack-i18n-transform';
+exports.webpackPluginName = Object.keys(dependencies)[0];
